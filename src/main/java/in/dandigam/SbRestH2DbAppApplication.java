6aa6d975dcdd3ab589c87e021d4f711d88a63bfd
dandigam <dandigam@gail.com>
2022-07-09 21:59:14 +0530

    add project

---
+++ src/main/java/in/dandigam/SbRestH2DbAppApplication.java
@@ -0,0 +1,13 @@
+package in.dandigam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRestH2DbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRestH2DbAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/dandigam/SbRestH2DbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.dandigam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbRestH2DbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
